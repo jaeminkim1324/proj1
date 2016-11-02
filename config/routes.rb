5b4e6be2f0b194044d11585ed015703ee2bcb0cc
Jae Min Kim <jaeminkim1324@berkeley.edu>
2016-11-02 14:16:26 -0700

    project largely done, need to finish readme

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+  resources :pokemons
+  patch '/capture', to: "pokemons#capture", as:'capture'
+  patch '/damage', to: "pokemons#damage", as: 'damage'
+  get '/new', to: "pokemons#new", as: 'new'
+  post '/create', to: "pokemons#create", as: 'create'
 end
